4ab7a1ce786cafdad9c3563750c8fad08165c8de
test <test@gmail.com>
2019-06-07 21:48:35 +0900

    publish

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19036010d19fac37d677690477f4340e.js"
+  "/precache-manifest.b7fef82902834f6c5d037bc6376a9aea.js"
 );
 
 workbox.clientsClaim();
